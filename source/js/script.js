212a24a1f5fb38e81a8c565158fb4fb28b81e5d6
nickbazie <nico@alwaysmarbella.com>
2019-05-07 23:16:53 +0200

    feat (module5-task2) add all from module5-task2

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,10 @@
 var pageHeader = document.querySelector('.page-header');
+var dealersMap = document.querySelector('.dealers__map');
 var menuWrapper = document.querySelector('.page-header__menu-wrapper');
 var navToggle = document.querySelector('.page-header__toggle');
 
 pageHeader.classList.remove('page-header--no-js');
+dealersMap.classList.remove('dealers__map--no-js');
 
 navToggle.addEventListener('click', function() {
   if (menuWrapper.classList.contains('page-header__menu-wrapper--closed')) {
