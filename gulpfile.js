2099461e9e8218107365a0a868c9dda29a96b798
nickbazie <nico@alwaysmarbella.com>
2019-05-07 21:15:41 +0200

    feat (JSON) add gulp-svgstore

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -41,6 +42,15 @@ gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
